6945dcf8501a2314f206b8cad2a0204d57a9e63f
Keerthi vasan s <keerthikv09752@gmail.com>
2026-05-04 22:35:05 +0530

    app.py

--- movie_sentiment_advanced/app.py
+++ movie_sentiment_advanced/app.py
@@ -16,5 +16,5 @@ def home():
     return render_template("index.html",prediction=prediction, accuracy=accuracy)
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
 

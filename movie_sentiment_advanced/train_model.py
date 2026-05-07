ff452ee1af7bb1aacf3959cbf92a7e4eb81a1491
Keerthi vasan s <keerthikv09752@gmail.com>
2026-05-07 23:37:00 +0530

    Update train_model.py

--- movie_sentiment_advanced/train_model.py
+++ movie_sentiment_advanced/train_model.py
@@ -6,21 +6,66 @@ import matplotlib.pyplot as plt
 import pickle
 
 texts = [
-    "this movie is amazing","I love this film","fantastic experience",
-    "worst movie ever","very boring and dull","terrible storyline",
-    "great acting and story","bad direction","awesome movie","not good"
-]*50
 
-labels = ["positive","positive","positive",
-          "negative","negative","negative",
-          "positive","negative","positive","negative"]*50
+    "this movie is amazing",
+    "excellent acting and story",
+    "i loved this movie",
+    "fantastic film",
+    "wonderful experience",
+    "great direction",
+    "super hit movie",
+    "awesome screenplay",
+    "best movie ever",
+    "very entertaining",
+    "good movie",
 
-vectorizer = TfidfVectorizer()
+    "worst movie ever",
+    "terrible acting",
+    "boring film",
+    "bad screenplay",
+    "waste of time",
+    "very disappointing",
+    "poor direction",
+    "awful movie",
+    "not good",
+    "horrible experience"
+
+]
+
+labels = [
+
+    "positive",
+    "positive",
+    "positive",
+    "positive",
+    "positive",
+    "positive",
+    "positive",
+    "positive",
+    "positive",
+    "positive",
+    "positive",
+
+    "negative",
+    "negative",
+    "negative",
+    "negative",
+    "negative",
+    "negative",
+    "negative",
+    "negative",
+    "negative",
+    "negative"
+]
+vectorizer = TfidfVectorizer(
+    stop_words='english',
+    lowercase=True
+)
 X = vectorizer.fit_transform(texts)
 
 X_train,X_test,y_train,y_test = train_test_split(X,labels,test_size=0.2)
 
-model = MultinomialNB()
+model = MultinomialNB(alpha=0.5)
 model.fit(X_train,y_train)
 
 pred = model.predict(X_test)

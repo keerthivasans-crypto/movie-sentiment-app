a0e663f447a2158709b1735b5663cd86184d71d4
Keerthi vasan s <keerthikv09752@gmail.com>
2026-05-08 14:34:34 +0530

    Update app.py

--- movie_sentiment_advanced/app.py
+++ movie_sentiment_advanced/app.py
@@ -87,5 +87,10 @@ def chat():
 
     return {"reply": bot_reply}
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+
+    port = int(os.environ.get("PORT", 10000))
+
+    app.run(host="0.0.0.0", port=port)
